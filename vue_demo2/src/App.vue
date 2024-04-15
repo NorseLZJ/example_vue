4138b4ccdb5646f7361de6a7f555bfdc2a1f3999
NorseLZJ <18841685054@163.com>
2024-04-15 17:57:26 +0800

    add

--- vue_demo2/src/App.vue
+++ vue_demo2/src/App.vue
@@ -7,17 +7,44 @@
       <el-header class="h-header">Header</el-header>
       <el-main>
 
-      <el-row :gutter="10" class="centered-row">
-			  <el-col :span="5"><div class="grid-content ep-bg-purple"> 翻译 </div></el-col>
-			  <el-col :span="5"><div class="grid-content ep-bg-purple"> 生成图片 </div></el-col>
-			  <el-col :span="5"><div class="grid-content ep-bg-purple"> 生成分镜 </div></el-col>
-			  <el-col :span="5"><div class="grid-content ep-bg-purple"> 生成音乐 </div></el-col>
-		  </el-row>
+        <el-row :gutter="11" class="centered-row">
+
+          <el-col :span="6">
+            <router-link to="/translation" class="grid-link">
+              <div class="grid-content">
+                <img src="./assets/1111.jpeg" alt="Translation">
+                <span class="text-overlay">翻译</span>
+              </div>
+            </router-link>
+          </el-col>
+
+          <el-col :span="6">
+            <div class="grid-content">
+              <img src="./assets/2222.png" alt="Generate Image">
+              <span class="text-overlay">生成图片</span>
+            </div>
+          </el-col>
+
+          <el-col :span="6">
+            <div class="grid-content">
+              <img src="./assets/l3.jpeg" alt="Generate Storyboard">
+              <span class="text-overlay">生成分镜</span>
+            </div>
+          </el-col>
+
+          <el-col :span="6">
+            <div class="grid-content">
+              <img src="./assets/l4.jpeg" alt="Generate Music">
+              <span class="text-overlay">生成音乐</span>
+            </div>
+          </el-col>
+
+        </el-row>
 
       </el-main>
     </el-container>
 
-    </div>
+  </div>
 </template>
 
 <style scoped>
@@ -25,16 +52,19 @@
   display: flex;
   justify-content: center;
 }
-.h-header{
+
+.h-header {
   background-color: blanchedalmond;
 }
 
 .el-row {
   margin-bottom: 10px;
 }
+
 .el-row:last-child {
   margin-bottom: 0;
 }
+
 .el-col {
   border-radius: 4px;
 }
@@ -42,12 +72,37 @@
 .grid-content {
   border-radius: 4px;
   min-height: 36px;
-  height: 300px; /* 设置高度为100px */
-  width: 300px; /* 设置宽度为100px，以确保是正方形 */
+  height: 300px;
+  width: 300px;
   display: flex;
   justify-content: center;
   align-items: center;
-  background-color: rgb(0, 128, 17);
+  position: relative;
+}
+
+.grid-content img {
+  width: 100%;
+  height: 100%;
+  object-fit: cover;
+  border-radius: 4px;
+}
+
+.text-overlay {
+  position: absolute;
+  top: 50%;
+  /* 将文字的顶部定位在父容器的50%位置 */
+  left: 50%;
+  /* 将文字的左侧定位在父容器的50%位置 */
+  transform: translate(-50%, -50%);
+  /* 使用transform属性将文字向上和向左移动自身宽高的一半，从而使文字在父容器中垂直和水平居中 */
   color: white;
+  font-size: 33px;
+  font-weight: bold;
+}
+
+.grid-link {
+  display: block;
+  text-decoration: none;
+  color: inherit;
 }
 </style>
